1c4a79b379b1c8b025578242fd4d3a2ae3432cec
unknown <sksmsspdlvmf@naver.com>
2023-03-19 03:21:15 +0900

    test

---
+++ wp-content/reactpress/apps/celesteaura/src/App3.js
@@ -0,0 +1,11 @@
+import Contact from "./pages/Contact"
+
+function App3() {
+  return (
+    <div className="App">
+		<Contact />
+    </div>
+  );
+}
+
+export default App3;
--- wp-content/reactpress/apps/celesteaura/src/index.js
+++ wp-content/reactpress/apps/celesteaura/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import App2 from "./App2";
+import App3 from "./App3";
 
 const root = document.getElementById("root")
   ? ReactDOM.createRoot(document.getElementById("root"))
@@ -11,6 +12,11 @@ const root = document.getElementById("root")
 
 const root2 = document.getElementById("root2")
   ? ReactDOM.createRoot(document.getElementById("root2"))
+  : null;
+
+
+  const contact = document.getElementById("contact")
+  ? ReactDOM.createRoot(document.getElementById("contact"))
   : null;
 
 if (root) {
@@ -25,6 +31,14 @@ if (root2) {
   root2.render(
     <React.StrictMode>
       <App2 />
+    </React.StrictMode>
+  );
+}
+
+if (contact) {
+  contact.render(
+    <React.StrictMode>
+      <App3 />
     </React.StrictMode>
   );
 }
